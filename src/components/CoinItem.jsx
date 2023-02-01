ccc66db812393109915c422c7fc9d6cbe5ef3d9d
Sravan T P <sravantp03@gmail.com>
2023-02-01 12:03:54 +0530

    styled Coin details section

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -4,20 +4,22 @@ import { Sparklines, SparklinesBars, SparklinesLine } from 'react-sparklines';
 
 const CoinItem = ({coin}) => {
   return (
-    <tr>
+    <tr className='h-[80px] border-b overflow-hidden'>
         <td><AiOutlineStar /></td>
         <td>{coin.market_cap_rank}</td>
         <td>
-            <div>
-                <img src={coin.image} alt={coin.id} width='20px' />
-                <p>{coin.name}</p>
+            <div className='flex items-center gap-2'>
+                <img src={coin.image} alt={coin.id} className='w-6 rounded-full' />
+                <p className='hidden sm:table-cell'>{coin.name}</p>
             </div>
         </td>
-        <td>{coin.symbol}</td>
-        <td>{coin.current_price}</td>
-        <td>{coin.price_change_percentage_24h}</td>
-        <td>{coin.total_volume}</td>
-        <td>{coin.market_cap}</td>
+        <td className='uppercase'>{coin.symbol}</td>
+        <td>${coin.current_price.toLocaleString()}</td>
+        <td>
+            {coin.price_change_percentage_24h > 0 ? (<span className='text-green-600'>{coin.price_change_percentage_24h.toFixed(2)}%</span>): (<span className='text-red-600'>{coin.price_change_percentage_24h.toFixed(2)}%</span>) }
+        </td>
+        <td className='w-[180px] hidden md:table-cell'>{coin.total_volume.toLocaleString()}</td>
+        <td className='w-[180px] hidden sm:table-cell'>{coin.market_cap.toLocaleString()}</td>
         <td>
             <Sparklines data={coin.sparkline_in_7d.price}>
             <SparklinesLine color="teal" />
--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -10,26 +10,26 @@ const CoinSearch = ({coins}) => {
   }
 
   return (
-    <div>
-        <div className='flex justify-between items-center'>
-            <h1>Search Crypto</h1>
+    <div className='rounded-div my-4'>
+        <div className='flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right'>
+            <h1 className='text-2xl font-bold my-2'>Search Crypto</h1>
             <form>
-                <input type='text' placeholder='search a coin' onChange={handleChange} />
+                <input type='text' placeholder='search a coin' onChange={handleChange} className='w-full bg-primary border px-4 py-2 rounded-2xl shadow-xl' />
             </form>
         </div>
 
         {/* Displaying coin details here */}
-        <table>
+        <table className='w-full border-collapse text-center'>
           <thead>
-            <tr>
+            <tr className='border-b'>
               <th></th>
-              <th>#</th>
-              <th>Coin</th>
+              <th className='px-4'>#</th>
+              <th className='text-left'>Coin</th>
               <th></th>
               <th>Price</th>
               <th>24h</th>
-              <th>24h Volume</th>
-              <th>Mkt</th>
+              <th className='hidden md:table-cell'>24h Volume</th>
+              <th className='hidden sm:table-cell'>Mkt</th>
               <th>Last 7 Days</th>
             </tr>
           </thead>
