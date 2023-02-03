1713fe94eab2fd6c1309020b1dadac5ef5ed4521
Sravan T P <sravantp03@gmail.com>
2023-02-03 13:56:01 +0530

    styled account page

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -3,24 +3,24 @@ import SavedCoins from '../components/SavedCoins';
 
 const Account = () => {
   return (
-    <div>
+    <div className='max-w-[1140px] mx-auto'>
       {/* Account section */}
-      <div className='rounded-div my-10 flex justify-between items-center py-4'>
+      <div className='rounded-div my-12 flex justify-between items-center py-8'>
         <div>
-          <h1 className='font-bold'>Account</h1>
+          <h1 className='font-bold text-2xl'>Account</h1>
           <div>
             <p>Welcome, User</p>
           </div>
         </div>
         <div>
-          <button>Sign out</button>
+          <button className='bg-button font-bold text-btnText border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl'>Sign out</button>
         </div>
       </div>
 
       {/* WatchList section */}
-      <div className='rounded-div'>
-        <div>
-          <h1 className='font-bold'>Watch List</h1>
+      <div className='rounded-div flex justify-between items-center my-12 py-8'>
+        <div className='w-full min-h-[300px]'>
+          <h1 className='font-bold text-2xl py-4'>Watch List</h1>
           <SavedCoins />
         </div>
       </div>
