c0c8413d7eddd61136b26b89ec62e0f1c156c332
Sravan T P <sravantp03@gmail.com>
2023-02-03 11:59:30 +0530

    added Signin page skeleton

--- src/routes/Signin.jsx
+++ src/routes/Signin.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { AiOutlineLock, AiOutlineMail } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 const Signin = () => {
   return (
-    <div>Signin</div>
+    <div className='rounded-div mt-10'>
+      <div className=''>
+        <h1>Sign In</h1>
+        <form>
+          <div>
+            <label>Email</label>
+            <div>
+              <input type="email" />
+              <AiOutlineMail />
+            </div>
+          </div>
+          <div>
+            <label>Password</label>
+            <div>
+              <input type="password" />
+              <AiOutlineLock />
+            </div>
+          </div>
+          <button>Sign in</button>
+        </form>
+        <p>Don't have an account?<Link to='/signup' className='text-blue-500'> Sign up</Link></p>
+      </div>
+    </div>
   );
 }
 
