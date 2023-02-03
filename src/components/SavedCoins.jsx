97cc2ca0f8e4a4e57bfd25f1dd87daf216efada6
Sravan T P <sravantp03@gmail.com>
2023-02-03 12:25:57 +0530

    added account page skeleton

---
+++ src/components/SavedCoins.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SavedCoins = () => {
+  return (
+    <div>SavedCoins</div>
+  );
+}
+
+export default SavedCoins;
--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import SavedCoins from '../components/SavedCoins';
 
 const Account = () => {
   return (
-    <div>Account</div>
+    <div>
+      {/* Account section */}
+      <div>
+        <div>
+          <h1>Account</h1>
+          <div>
+            <p>Welcome, User</p>
+          </div>
+        </div>
+        <div>
+          <button>Sign out</button>
+        </div>
+      </div>
+
+      {/* WatchList section */}
+      <div>
+        <div>
+          <h1>Watch List</h1>
+          <SavedCoins />
+        </div>
+      </div>
+    </div>
   );
 }
 
